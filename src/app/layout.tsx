6097d54c2738766243a6b7bc8903cac446398ea5
Ana Paula <132491694+ribbeiroana@users.noreply.github.com>
2024-11-28 16:13:46 -0300

    docs(files): files

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Spotify",
+  description: "Listen to music",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className='bg-zinc-900 text-zinc-50'>
+        {children}
+      </body>
+    </html>
+  );
+}
